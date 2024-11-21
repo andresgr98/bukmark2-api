f49f851b611caf71389f64a208983722e03c6e7e
andresgr98 <gomezand15@gmail.com>
2024-11-21 12:46:18 +0100

    Docker config WIP

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+composer install
+
+while true; do
+    
+    output=$(symfony console doctrine:migrations:migrate --no-interaction 2>&1)
+    
+    echo "$output"
+
+    if [[ "$output" == *"The version \"latest\" couldn't be reached"* ]]; then
+        echo "No registered migrations. Exiting."
+        break
+    fi
+    
+    if [[ "$output" == *"[OK]"* ]]; then
+        echo "Migrations completed successfully."
+        break
+    fi
+
+    echo "Migrations not completed. Retrying in 5 seconds..."
+    sleep 5
+done
+
+symfony server:start --no-tls --allow-http --port=8000
